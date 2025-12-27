3bcae19121796bef35931a190b9fe73a75bec8f5
myakalavidyasagar <vidyasagar.myakala@gmail.com>
2025-12-27 15:12:43 +0530

    Added Swagger configuration

---
+++ EcommerceSpringBootProject/src/main/java/com/api/rest/springboot/ecommerce/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.api.rest.springboot.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	OpenAPI customOpenAPI() {
+		return new OpenAPI().info(new Info() .title("Ecommerce SpringBoot API Documentation")
+                .version("1.0")
+                .description("API documentation for Ecommerce SpringBoot API application"));
+	}
+}
