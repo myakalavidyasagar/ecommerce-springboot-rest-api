3717f9387a55e94ba285d6903dcb135dc1a7299c
myakalavidyasagar <vidyasagar.myakala@gmail.com>
2025-12-27 15:48:04 +0530

    Modified Content for swagger doc in meaningful

--- EcommerceSpringBootProject/src/main/java/com/api/rest/springboot/ecommerce/config/SwaggerConfig.java
+++ EcommerceSpringBootProject/src/main/java/com/api/rest/springboot/ecommerce/config/SwaggerConfig.java
@@ -8,14 +8,15 @@ import io.swagger.v3.oas.models.info.Info;
 
 	@Configuration
 	public class SwaggerConfig {
-
+		
 	    @Bean
 	    OpenAPI customOpenAPI() {
 	        return new OpenAPI()
 	                .info(new Info()
-	                        .title("AddToCartToOrder API Documentation")
+	                        .title("ECommerce Springboot API Documentation")
 	                        .version("1.0")
-	                        .description("API documentation for CartToOrder application"));
+	                        .description("API documentation for ECommerce Springboot API"));
+	        
 	    }
 	}
 
